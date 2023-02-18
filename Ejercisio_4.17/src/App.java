643dfe389f69f3e0a9c7e66b4fbf452ea4c76d24
Alejandro-Mejia1999 <chandyjm1999@gmail.com>
2023-02-18 13:20:28 -0600

    casi terminado

--- Ejercisio_4.17/src/App.java
+++ Ejercisio_4.17/src/App.java
@@ -3,16 +3,31 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) throws Exception {
-        int x, Km, Lt;
+        int x, ResulTK, ResulTL;
+        ResulTK = 0;
+        ResulTL = 0;
+        float resul;
+
         Scanner entra = new Scanner(System.in);
         System.out.println("digite el numero de viajes realizados");
         x = entra.nextInt();
-        for (int i = 1; i <= x; i++) {
+        float[] viaje = new float[x];
+        int[] Km = new int[x];
+        int[] Lt = new int[x];
+        for (int i = 0; i < x; i++) {
             System.out.println("cuantos kilometros en el viaje  " + i);
-            Km = entra.nextInt();
+            Km[i] += entra.nextInt();
             System.out.println("cuantos litros en el viaje  " + i);
-            Lt = entra.nextInt();
+            Lt[i] += entra.nextInt();
+            ResulTK += Km[i];
+            ResulTL += Lt[i];
         }
+        for (int j = 0; j < x; j++) {
+            viaje[j] = Km[j] / Lt[j];
+            System.out.println("\t\t\tEn el viaje " + j + " se consumio: " + viaje[j] + "Km/Lt");
+        }
+        resul = (ResulTK / ResulTL);
+        System.out.println("\n\n\t\t\tel porsentaje de consumo total es: " + resul + "Km/Lt\n\n");
         entra.close();
     }
 }
